6d6e44f405f7a664a561edd3d9fa8da65d5f045c
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-04-12 09:46:44 -0300

    Criando repositório GrupoDeFornecedoresRepository

---
+++ src/main/java/br/com/zup/edu/fornecedormanager/repository/GrupoDeFornecedoresRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.edu.fornecedormanager.repository;
+
+import br.com.zup.edu.fornecedormanager.model.GrupoDeFornecedores;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GrupoDeFornecedoresRepository extends JpaRepository<GrupoDeFornecedores, Long> {
+}
